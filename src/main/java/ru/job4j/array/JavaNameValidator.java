f0439e3184c7c01e51dc8974ca2caf5528ed5f5a
kuvalda <hammer1505@yandex.ru>
2022-08-19 18:01:39 +0300

    6.9. String - это массив [#504884]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -2,19 +2,16 @@ package ru.job4j.array;
 
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
-        boolean valid = false;
-        if (!name.isEmpty()) {
-            if (isLowerLatinLetter(name.charAt(0))) {
-                for (int i = 1; i < name.length(); i++) {
-                    int code = name.codePointAt(i);
-                    if (!isSpecialSymbol(code)
-                            && !isUpperLatinLetter(code)
-                            && !isLowerLatinLetter(code)
-                            && !Character.isDigit(code)) {
-                        valid = false;
-                        break;
-                    }
-                    valid = true;
+        boolean valid = (!name.isEmpty() && (isLowerLatinLetter(name.charAt(0))));
+        if (valid) {
+            for (int i = 1; i < name.length(); i++) {
+                int code = name.codePointAt(i);
+                if (!isSpecialSymbol(code)
+                        && !isUpperLatinLetter(code)
+                        && !isLowerLatinLetter(code)
+                        && !Character.isDigit(code)) {
+                    valid = false;
+                    break;
                 }
             }
         }
@@ -26,10 +23,10 @@ public class JavaNameValidator {
     }
 
     public static boolean isUpperLatinLetter(int code) {
-        return Character.isUpperCase(code);
+        return (code >= 65 && code <= 90);
     }
 
     public static boolean isLowerLatinLetter(int code) {
-        return Character.isLowerCase(code);
+        return (code >= 97 && code <= 122);
     }
 }
